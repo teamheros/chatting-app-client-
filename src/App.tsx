4cee545a03159d4ba8c442af58a0b4b8ed0f7518
Saurav <sauravtewatia93@gmail.com>
2021-05-30 14:03:51 +0530

    socket practice

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import "./App.css";
 import MainPage from "./components/mainPage";
 import { Provider } from "react-redux";
 import store from "./redux/redux";
+import Practice from "./practice/practice";
 
 function App() {
   return (
     <Provider store={store}>
-      <MainPage />
+      {/* <MainPage /> */}
+      <Practice />
     </Provider>
   );
 }
