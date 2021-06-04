f1fa56a015521edadc695e3d355a14287188dade
Saurav <sauravtewatia93@gmail.com>
2021-06-04 23:48:04 +0530

    re-push

--- src/services/chatServices.ts
+++ src/services/chatServices.ts
@@ -3,13 +3,13 @@ import { USERS_CHAT } from "../redux/actionTypes";
 
 export const getChats = async (dispatch: any, users: any) => {
   let res = await axios.get(
-    "http://localhost:4001/api/chat/" + JSON.stringify(users)
+    "http://localhost:4000/api/chat/" + JSON.stringify(users)
   );
   dispatch({ type: USERS_CHAT, payload: res.data.messages });
 };
 
 export const sendMessage = async (dispatch: any, users: any, msg: string) => {
-  let res = await axios.post("http://localhost:4001/api/chat", {
+  let res = await axios.post("http://localhost:4000/api/chat", {
     users,
     msg,
     sentBy: "saurav",
